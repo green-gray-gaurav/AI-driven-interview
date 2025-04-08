345d93a3f097175af46f2dd34655464c20a8e307
= <=>
2025-04-09 01:25:08 +0530

    info add

--- scoring_system.py
+++ scoring_system.py
@@ -2,17 +2,17 @@
 class EasyScoring():
     def __init__(self):
         pass
-    def getScore(self, score_from_resume , score_from_interview):
+
+    def getScore(self, score_from_resume, score_from_interview):
         # for a qusetion we get these scores
         # {
         #   "Accuracy": 1,
         #   "Relevance": 5,
         #   "Clarity": 4,
         # }
-        # and we can compute G-index = accuracy * clarity + relevance
+        # and we can compute G-index = accuracy * relevance + clarity
         # or we can compte total_score = weighted_avg([accuracy, clarity, relevance], weights=[0.5, 0.25, 0.25])
         # total score is what used in further computations
-
 
         # let assume we got techinal, aptitude, concept calrity question so we calculate the score for them
 
@@ -22,16 +22,14 @@ class EasyScoring():
         aptitude_score = "Avg. score from aptitude answers"
         clarity_score = " Avg. score from concept clarity answers"
 
-
         # --------------from resume score
-
 
         technical_match = "Resume-job match score (0 to 1 or 0–5 scaled to 0–1)"
         clarity_match = " How confidently and clearly the candidate answers (could be model-rated, 0–1)"
 
-        ##-----------from resume you can also get 
+        # -----------from resume you can also get
         experience = None
-        ##-----------from techincal round you can also get communcation skills -------s
+        # -----------from techincal round you can also get communcation skills -------s
         # way of speaking
         # grammar
         # fluency of language
@@ -43,17 +41,16 @@ class EasyScoring():
         aug_technical_score = technical_score * technical_match
         aug_clarity_score = clarity_score * clarity_match
 
-
         mind_fitness_score = weighted_average([
             aug_technical_score,
             aptitude_score,
             aug_clarity_score
         ], weights=[0.4, 0.3, 0.3])
 
+        candidate_score_vector = [mind_fitness_score,
+                                  experience, communication_score]
 
-        candidate_score_vector = [mind_fitness_score , experience , communication_score]
-
-        #-----------standardist the vector values in 0-5 range 
+        # -----------standardist the vector values in 0-5 range
 
         return candidate_score_vector
         # this vector is capable of giving the complete cinforamtion
